f99646b0fa0ef492df38509bbaccf5f8b66960e8
toeten <n.santiago0228@gmail.com>
2024-04-03 16:57:40 -0400

    finished question 2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -16,8 +16,26 @@ measureRain(0)
 measureRain(2)
 measureRain(3)
 
-const happyBirthdayPet = () => {
+const happyBirthdayPet = (breed, age) => {
+  if (breed === 'snake') {
+    console.log('Hiss hiss!')
+  } else if (breed === 'cat' & age < 5) {
+    console.log('Mew mew!')
+  } else if (breed === 'cat' && age >= 5) {
+    console.log('Meow meow!')
+  } else if (breed === 'dog' && age < 5) {
+    console.log('Arf arf!')
+  } else if (breed === 'dog' && age >= 5 && age < 10) {
+    console.log('Woof woof!')
+  } else if (breed === 'dog' && age >= 10) {
+    console.log('Boof!')
+  } else {
+    console.log('Happy birthday!')
+  }
 };
+
+happyBirthdayPet('snake', 0)
+happyBirthdayPet('cat', 2)
 
 const funTypes = () => {
 };
--- src/playground.js
+++ src/playground.js
@@ -33,3 +33,29 @@ measureRain(2)
 //   returnPositiveNegativeZero,
 // } = require('./debug');
 // const { wildlyBiasedReview } = require('./modify');
+
+
+
+
+
+//FizzBuzz: Write a function that takes in a number: 
+//if the number is divisible by three, return "Fizz"
+//if the number is divisible by 5, return "Buzz"
+//if divisible by 3 and by 5, return "FizzBuzz"
+
+const fizzBuzz = (number) => {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "FizzBuzz"
+    } else if (number % 3 === 0) {
+        return "Fizz"
+    } else if (number % 5 === 0) {
+        return "Buzz"
+    } else {
+        return "Not divisible by 3 OR 5."
+    }
+}
+
+console.log(fizzBuzz(10))
+console.log(fizzBuzz(23))
+console.log(fizzBuzz(9))
+console.log(fizzBuzz(15))
