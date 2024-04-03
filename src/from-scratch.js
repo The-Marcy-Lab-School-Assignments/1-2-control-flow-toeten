1feb4482fd4e07c4a383a7d4c36f5ed0d62b804c
toeten <n.santiago0228@gmail.com>
2024-04-03 14:45:02 -0400

    finished question 1

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,20 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    console.log('drought')
+  } else if (inches < 2) {
+    console.log('dry')
+  } else if (inches < 4) {
+    console.log('average')
+  } else if (inches < 6) {
+    console.log('rainy')
+  } else {
+    console.log('flood')
+  }
 };
+
+measureRain(0)
+measureRain(2)
+measureRain(3)
 
 const happyBirthdayPet = () => {
 };
--- src/playground.js
+++ src/playground.js
@@ -1,5 +1,25 @@
 // const {
 //   measureRain,
+const measureRain = (inches) => {
+    switch (true) {
+        case inches === 0:
+            console.log('drought');
+            break;
+        case inches < 2:
+            console.log('dry');
+            break;
+        case inches < 4:
+            console.log('average');
+            break;
+        case inches < 6:
+            console.log('rainy');
+            break;
+        default:
+            console.log('flood');
+    }
+};
+
+measureRain(2)
 //   happyBirthdayPet,
 //   funTypes,
 //   rounder,
